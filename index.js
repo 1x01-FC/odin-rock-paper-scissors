6905f33cc4c1912db2777560b9d065614ef49103
toonvesper <toonvesper@gmail.com>
2023-08-07 19:06:36 -0400

    add 5 games with tracker function

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-function getComputerChoice() {
-    let array = ["rock", "paper", "scissors"];
+const validChoices = ["rock", "paper", "scissors"];
 
-    return array[Math.floor(Math.random() * 3)]
+
+function getComputerChoice() {
+    return validChoices[Math.floor(Math.random() * 3)]
 }
 
 
-function play(userChoice) {
+function playOneRound(playerInput) {
+    let playerChoice = playerInput.toLowerCase();
     let computerChoice = getComputerChoice();
     let result;
 
-    switch (userChoice) {
+    switch (playerChoice) {
         case computerChoice:
-            result = "Draw"
+            result = `Draw! Both players selected ${computerChoice}.`
             break;
         case "rock":
             if (computerChoice === "paper") {
-                result = "You Lose."
+                result = "You Lose! Paper beats rock."
             }
             else {
-                result = "You Win!"
+                result = "You Win! Rock beats scissors."
             }
             break;
         case "paper":
-            if (computerChoice === "rock") {
-                result = "You Lose."
+            if (computerChoice === "scissors") {
+                result = "You Lose! Scissors beats paper. "
             } else {
-                result = "You Win!"
+                result = "You Win! Paper beats rock."
             }
             break;
         case "scissors":
             if (computerChoice === "rock") {
-                result = "You Lose."
+                result = "You Lose! Rock beats scissors"
             } else {
-                result = "You Win!"
+                result = "You Win! Scissors beats paper"
             }
             break;
         default:
@@ -42,4 +44,35 @@ function play(userChoice) {
 
     return result;
 
+}
+
+function game() {
+    let scoreLog = [];
+
+    for (let round = 0; round < 5; round++) {
+        let playerChoice;
+
+        do {
+            playerChoice = prompt("Rock, paper or scissors?").toLowerCase();
+            if (!validChoices.includes(playerChoice)) {
+                alert("Invalid choice! Please choose from 'rock', 'paper', or 'scissors'.");
+            }
+        } while (!validChoices.includes(playerChoice));
+
+        let result = playOneRound(playerChoice);
+        console.log(result);
+        scoreLog.push(result);
+    }
+
+    let winCount = scoreLog.filter((log) => log.includes("Win")).length;
+    let lossCount = scoreLog.filter((log) => log.includes("Lose")).length;
+    let drawCount = 5 - (winCount + lossCount);
+
+    if (winCount > lossCount) {
+        return `You won ${winCount} - ${lossCount} with ${drawCount} draws.`
+    } else if (winCount < lossCount) {
+        return `You lost ${winCount} - ${lossCount} with ${drawCount} draws.`
+    } else {
+        return `You tied at ${winCount} - ${lossCount} with ${drawCount} draws.`
+    }
 }
